a5014128a7c0485f4e93264f173d39b37a4d229d
Alexander Tyapkov <tyapkov@gmail.com>
2016-02-24 14:23:39 +0100

    move urls and views from profile to messages

--- insite_messages/urls.py
+++ insite_messages/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from insite_messages import views
 
 urlpatterns = [
 
+    url(r'^(?P<username>[\@\.\w-]+)/messages/$', views.messages, name='messages'),
+    url(r'^(?P<username>[\@\.\w-]+)/messages/write/$', views.message_write, name='message_write'),
+    url(r'^(?P<username>[\@\.\w-]+)/messages/(?P<message_id>[0-9]+)/$', views.message_view, name='message_view'),
+    url(r'^(?P<username>[\@\.\w-]+)/messages/(?P<message_id>[0-9]+)/reply$', views.message_reply, name='message_reply'),
+    url(r'^(?P<username>[\@\.\w-]+)/messages/(?P<message_id>[0-9]+)/remove$', views.message_remove, name='message_remove'),
 ]

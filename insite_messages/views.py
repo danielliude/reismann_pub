7f269a1e9afd486e132825ce2fab09ef77457e2e
Alexander Tyapkov <tyapkov@gmail.com>
2016-03-21 12:56:42 +0300

    sercurity for messages views

--- insite_messages/views.py
+++ insite_messages/views.py
@@ -22,19 +22,25 @@ def messages(request, username,
                 template_name='profiles/messages.html',
                 extra_context=None, **kwargs):
 
-  user = get_object_or_404(User, username__iexact=username)
-  profile = get_user_profile(user)
-  messages = Message.objects.all_for(user)
+  if request.user.username == username:
 
-  if not extra_context: extra_context = dict()
-  extra_context['messages'] = messages
-  extra_context['profile'] = profile
+      user = get_object_or_404(User, username__iexact=username)
+      profile = get_user_profile(user)
+      messages = Message.objects.all_for(user)
 
-  extra_context = makeContextForDetails(request, extra_context)
-  extra_context = makeContextForMessages(request, extra_context)
+      if not extra_context: extra_context = dict()
+      extra_context['messages'] = messages
+      extra_context['profile'] = profile
 
-  return ExtraContextTemplateView.as_view(template_name=template_name,
-                                          extra_context=extra_context)(request)
+      extra_context = makeContextForDetails(request, extra_context)
+      extra_context = makeContextForMessages(request, extra_context)
+
+      return ExtraContextTemplateView.as_view(template_name=template_name,
+                                              extra_context=extra_context)(request)
+  else:
+
+    url = reverse('profiles:messages', kwargs={'username':request.user.username})
+    return HttpResponseRedirect(url)
 
 
 @secure_required
@@ -43,37 +49,43 @@ def message_write(request, username, write_message_form=MessageComposeForm,
                 template_name='profiles/message_write.html', success_url=None,
                 extra_context=None, **kwargs):
 
-    user = get_object_or_404(User, username__iexact=username)
-    profile = get_user_profile(user)
-    contact = get_user_contact(user)
+    if request.user.username == username:
+        user = get_object_or_404(User, username__iexact=username)
+        profile = get_user_profile(user)
+        contact = get_user_contact(user)
 
-    form = write_message_form()
+        form = write_message_form()
 
-    if request.method == 'POST':
-        form = write_message_form(request.POST, request.FILES)
+        if request.method == 'POST':
+            form = write_message_form(request.POST, request.FILES)
 
-        if form.is_valid():
-          message = form.save(user)
-          message.sent_at = datetime.utcnow().replace(tzinfo=utc)
-          message.send_notification_email_to_recipient()
-          message.save()
+            if form.is_valid():
+              message = form.save(user)
+              message.sent_at = datetime.utcnow().replace(tzinfo=utc)
+              message.send_notification_email_to_recipient()
+              message.save()
 
-          if success_url:
-            redirect_to = success_url
-          else:
-            redirect_to = reverse('profiles:messages', kwargs={'username': username})
-          return redirect(redirect_to)
+              if success_url:
+                redirect_to = success_url
+              else:
+                redirect_to = reverse('profiles:messages', kwargs={'username': username})
+              return redirect(redirect_to)
 
-    if not extra_context: extra_context = dict()
-    extra_context['form'] = form
-    extra_context['profile'] = profile
-    extra_context['contact'] = contact
+        if not extra_context: extra_context = dict()
+        extra_context['form'] = form
+        extra_context['profile'] = profile
+        extra_context['contact'] = contact
 
-    extra_context = makeContextForDetails(request, extra_context)
-    extra_context = makeContextForMessages(request, extra_context)
+        extra_context = makeContextForDetails(request, extra_context)
+        extra_context = makeContextForMessages(request, extra_context)
 
-    return ExtraContextTemplateView.as_view(template_name=template_name,
-                                          extra_context=extra_context)(request)
+        return ExtraContextTemplateView.as_view(template_name=template_name,
+                                              extra_context=extra_context)(request)
+
+    else:
+
+        url = reverse('profiles:message_write', kwargs={'username':request.user.username})
+        return HttpResponseRedirect(url)
 
 
 @secure_required
@@ -82,27 +94,32 @@ def message_view(request, username, message_id, write_message_form=MessageCompos
                  template_name='profiles/message_view.html', success_url=None,
                  extra_context=None, **kwargs):
 
-    user = get_object_or_404(User, username__iexact=username)
-    profile = get_user_profile(user)
+    if request.user.username == username:
 
-    message = Message.objects.get(pk= message_id)
+        user = get_object_or_404(User, username__iexact=username)
+        profile = get_user_profile(user)
 
-    if message.recipient == user:
-        message.read_at = datetime.now()
-        message.save()
+        message = get_object_or_404(Message, pk= message_id)
 
-    if not extra_context: extra_context = dict()
+        if message.recipient == user:
+            message.read_at = datetime.now()
+            message.save()
 
-    if user.has_perm('insite_messages.view_message', message):
-        extra_context['message'] = message
+        if not extra_context: extra_context = dict()
 
-    extra_context['profile'] = profile
+        if user.has_perm('insite_messages.view_message', message):
+            extra_context['message'] = message
 
-    extra_context = makeContextForDetails(request, extra_context)
-    extra_context = makeContextForMessages(request, extra_context)
+        extra_context['profile'] = profile
 
-    return ExtraContextTemplateView.as_view(template_name=template_name,
-                                          extra_context=extra_context)(request)
+        extra_context = makeContextForDetails(request, extra_context)
+        extra_context = makeContextForMessages(request, extra_context)
+
+        return ExtraContextTemplateView.as_view(template_name=template_name,
+                                              extra_context=extra_context)(request)
+    else:
+        url = reverse('profiles:message_view', kwargs={'username':request.user.username, 'message_id':message_id})
+        return HttpResponseRedirect(url)
 
 
 @secure_required
@@ -111,66 +128,79 @@ def message_reply(request, username, message_id, write_message_form=MessageCompo
                  template_name='profiles/message_write.html', success_url=None,
                  extra_context=None, **kwargs):
 
-    user = get_object_or_404(User, username__iexact=username)
+    if request.user.username == username:
 
-    profile = get_user_profile(user)
-    contact = get_user_contact(user)
-    message = Message.objects.get(pk= message_id)
+        user = get_object_or_404(User, username__iexact=username)
 
-    initial_subject = "Re: " + message.subject
-    initial_body = ">" + message.body
+        profile = get_user_profile(user)
+        contact = get_user_contact(user)
+        message = Message.objects.get(pk= message_id)
 
-    form = write_message_form(initial={'recipient': message.sender,
-                                       'subject': initial_subject,
-                                       'body': initial_body}
-                                )
+        initial_subject = "Re: " + message.subject
+        initial_body = ">" + message.body
+
+        form = write_message_form(initial={'recipient': message.sender,
+                                           'subject': initial_subject,
+                                           'body': initial_body}
+                                    )
 
 
-    if request.method == 'POST':
-        form = write_message_form(request.POST, request.FILES)
+        if request.method == 'POST':
+            form = write_message_form(request.POST, request.FILES)
 
-        if form.is_valid():
-          message = form.save(user)
-          message.save()
+            if form.is_valid():
+              message = form.save(user)
+              message.save()
 
-          if success_url:
-            redirect_to = success_url
-          else:
-            redirect_to = reverse('profiles:messages', kwargs={'username': username})
-          return redirect(redirect_to)
+              if success_url:
+                redirect_to = success_url
+              else:
+                redirect_to = reverse('profiles:messages', kwargs={'username': username})
+              return redirect(redirect_to)
 
-    if not extra_context: extra_context = dict()
-    extra_context['form'] = form
-    extra_context['profile'] = profile
-    extra_context['contact'] = contact
+        if not extra_context: extra_context = dict()
+        extra_context['form'] = form
+        extra_context['profile'] = profile
+        extra_context['contact'] = contact
 
-    extra_context = makeContextForDetails(request, extra_context)
-    extra_context = makeContextForMessages(request, extra_context)
+        extra_context = makeContextForDetails(request, extra_context)
+        extra_context = makeContextForMessages(request, extra_context)
 
-    return ExtraContextTemplateView.as_view(template_name=template_name,
-                                          extra_context=extra_context)(request)
+        return ExtraContextTemplateView.as_view(template_name=template_name,
+                                              extra_context=extra_context)(request)
+
+    else:
+
+        url = reverse('profiles:message_reply', kwargs={'username':request.user.username, 'message_id':message_id})
+        return HttpResponseRedirect(url)
 
 @secure_required
 @permission_required_or_403('insite_messages.delete_message')
 def message_remove(request, username, message_id, template_name='profiles/messages.html', success_url=None,
                  extra_context=None, **kwargs):
 
-  user = get_object_or_404(User, username__iexact=username)
-  message = Message.objects.get(pk = message_id)
+  if request.user.username == username:
 
-  if user.has_perm('delete_message', message):
+      user = get_object_or_404(User, username__iexact=username)
+      message = Message.objects.get(pk = message_id)
 
-      if(message.sender == user):
-          message.sender_deleted_at = datetime.now()
-          message.save()
+      if user.has_perm('delete_message', message):
 
-      if(message.recipient == user):
-          message.recipient_deleted_at = datetime.now()
-          message.save()
+          if(message.sender == user):
+              message.sender_deleted_at = datetime.now()
+              message.save()
 
-      if(message.recipient_deleted_at is not None and message.sender_deleted_at is not None):
-          message.delete()
+          if(message.recipient == user):
+              message.recipient_deleted_at = datetime.now()
+              message.save()
 
-  url = reverse('profiles:messages', kwargs={'username':user.username})
-  return HttpResponseRedirect(url)
+          if(message.recipient_deleted_at is not None and message.sender_deleted_at is not None):
+              message.delete()
+
+      url = reverse('profiles:messages', kwargs={'username':user.username})
+      return HttpResponseRedirect(url)
+
+  else:
+    url = reverse('profiles:message_remove', kwargs={'username':request.user.username, 'message_id':message_id})
+    return HttpResponseRedirect(url)
 

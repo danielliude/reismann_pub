242e8fb14bb482c910b160fa9ea54839d52dce2f
Alexander Tyapkov <tyapkov@gmail.com>
2016-05-02 13:42:47 +0200

    remove newcomer providers, new comer cities from index page

--- core/views.py
+++ core/views.py
@@ -29,19 +29,11 @@ def index(request):
                         .exclude(Q(user__is_staff=True) | Q(user__is_superuser=True)) \
                         .order_by('created_at')[:6], key=lambda x: random.random())
 
-      newcomer_profiles = Profile.objects.filter(settings__profile_is_active=True) \
-                            .exclude(Q(user__is_staff=True) | Q(user__is_superuser=True)) \
-                            .order_by('created_at')[:6]
-
-      newcomer_cities = City.objects.filter(is_active=True).order_by('created_at')[:6]
-
       popular_cities = sorted(City.objects.filter(is_active=True).order_by('created_at')[:6], key=lambda x: random.random())
 
       context = {
         'city_list' : city_list,
         'popular_profiles': popular_profiles,
-        'newcomer_profiles': newcomer_profiles,
-        'newcomer_cities': newcomer_cities,
         'popular_cities': popular_cities,
       }
 

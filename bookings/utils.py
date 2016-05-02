327c056cffe974beb0a47e46a53c813efee26b03
Alexander Tyapkov <tyapkov@gmail.com>
2016-05-02 13:38:42 +0200

    show correct number of bookings, click on bookings redirect to bookings page

--- bookings/utils.py
+++ bookings/utils.py
@@ -2,9 +2,9 @@ from bookings.models import Booking
 from django.shortcuts import get_object_or_404
 
 def get_number_bookings(user):
-    return 0
-    # Booking.objects.get_number_bookings(user)
-
+    bookings_to_me = Booking.objects.filter(recipient=user)
+    bookings_from_me = Booking.objects.filter(sender = user)
+    return len(bookings_to_me)+len(bookings_from_me)
 
 def get_booking_by_id(id):
     return get_object_or_404(Booking, pk = id)

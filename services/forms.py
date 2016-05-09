3180a4d1bf82e4859d3fb70f9319ef076127b9b6
Alexander Tyapkov <tyapkov@gmail.com>
2016-05-09 22:36:40 +0200

    max 5 cities

--- services/forms.py
+++ services/forms.py
@@ -88,8 +88,8 @@ class ServiceForm(forms.ModelForm):
 
   def clean_cities(self):
       value = self.cleaned_data['cities']
-      if len(value) > 3:
-          raise forms.ValidationError("You can't select more than 3 cities.")
+      if len(value) > 5:
+          raise forms.ValidationError("You can't select more than 5 cities.")
       return value
 
   def save(self, username, force_insert=False, force_update=False, commit=True):

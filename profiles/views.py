e6a9baecc7416e636c4b91b115797517773e3e5d
wangyongjie <745814097@qq.com>
2016-06-07 11:57:41 +0800

    dashborad page

--- profiles/views.py
+++ profiles/views.py
@@ -220,6 +220,19 @@ def dashboard(request, username, template_name='profiles/dashboard.html',
   extra_context = makeContextForDetails(request, extra_context)
   extra_context = makeContextForAllUserServices(user, extra_context)
 
+  limit = 10  # 每页显示的记录数
+  topics = extra_context['notifications']
+  paginator = Paginator(topics, limit)  # 实例化一个分页对象
+
+  page = request.GET.get('page')  # 获取页码
+  try:
+      topics = paginator.page(page)  # 获取某页对应的记录
+  except PageNotAnInteger:  # 如果页码不是个整数
+      topics = paginator.page(1)  # 取第一页的记录
+  except EmptyPage:  # 如果页码太大，没有相应的记录
+      topics = paginator.page(paginator.num_pages)  # 取最后一页的记录
+  extra_context['notifications'] = topics
+
   return ExtraContextTemplateView.as_view(template_name=template_name, extra_context=extra_context)(request)
 
 @secure_required
